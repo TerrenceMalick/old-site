f73807b5b8e2000289997bdb07157df69e4f8029
Michael Zhang <zhangmichael@berkeley.edu>
2014-11-11 21:14:33 -0800

    new expandable section, mobile fixes, new favicon

--- js/lazy-carousel.js
+++ js/lazy-carousel.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	//initializes a lazy loading carousel
 	$('.view').slick({
 	  lazyLoad: 'ondemand',
-	  slidesToShow: 3,
+	  slidesToShow: 1,
 	  slidesToScroll: 1,
 	  accessibility: true,
-	  centerMode: true
+	  fade: true
 	});
 });
